8033c8c706657726e098981c7ea41e41e3b20a2b
Abir Atur <22197855+abiratur@users.noreply.github.com>
2020-11-15 10:20:26 +0200

    add sample app

---
+++ samples/OrderApplication/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mictor;
+
+namespace OrderApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderId = Guid.NewGuid().ToString();
+
+            // simulate 2 requests
+
+            FirstRequest(orderId);
+            SecondRequest(orderId);
+
+            Console.ReadLine();
+        }
+
+        private static void FirstRequest(string orderId)
+        {
+            using (IActor orderActor = ActorPool.Shared.GetOrCreate(orderId))
+            {
+                orderActor.Enqueue(async () =>
+                {
+                    Console.WriteLine("Getting order from db...");
+
+                    await Task.Delay(5000);
+
+                    Console.WriteLine("Successfully got order from db");
+                });
+            }
+        }
+
+        private static void SecondRequest(string orderId)
+        {
+            using (IActor orderActor = ActorPool.Shared.GetOrCreate(orderId))
+            {
+                orderActor.Enqueue(() =>
+                {
+                    Console.WriteLine("Updating order");
+
+                    return Task.CompletedTask;
+                });
+            }
+        }
+    }
+}
