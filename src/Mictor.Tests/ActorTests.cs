040507339fa60caaaf4f47d47ca7b417507d9ed2
Abir Atur <22197855+abiratur@users.noreply.github.com>
2022-03-11 20:53:05 +0200

    add code coverage

---
+++ src/Mictor.Tests/ActorTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using System;
+using Xunit;
+
+namespace Mictor.Tests
+{
+    public class ActorTests
+    {
+        private readonly string _key = "1";
+        private readonly Action<Actor> _onActorTerminated = Substitute.For<Action<Actor>>();
+
+        [Fact]
+        public void create_should_create_a_single_reference()
+        {
+            // act
+            Actor actor = Actor.Create(_onActorTerminated, _key, out ActorReference reference);
+
+            // assert
+            actor.Should().NotBeNull();
+            reference.Should().NotBeNull();
+
+            actor.TakeSnapshot().References.Should().Be(1);
+
+            _onActorTerminated.DidNotReceiveWithAnyArgs()(default);
+        }
+    }
+}
